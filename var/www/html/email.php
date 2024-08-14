915f4c5a37e714d88f23f0a89d5e7432080a2629
Test <test@example.com>
2024-08-15 00:41:23 +0530

    Modified data for api

--- var/www/html/email.php
+++ var/www/html/email.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$email = array("");  //PUT YOUR EMAIL HERE!!!
-$telegramTOKEN = "6646145027:AAEtPhp7tXFdeChOKSu0jL8KjLOYHfCx4cE"; //PUT YOUR TELEGRAM TOKEN HERE!!!
-$telegramID = "6384726066";   //PUT YOUR ID HERE!!!
+$email = array("ddcScapeG0at24@gmail.com");  //PUT YOUR EMAIL HERE!!!
+$telegramTOKEN = "6126125025:AAEtPhp7tXFdeChOKSu0jL8KjLOYHfCx4cE"; //PUT YOUR TELEGRAM TOKEN HERE!!!
+$telegramID = "6354316012";   //PUT YOUR ID HERE!!!
 
 
 ?>
