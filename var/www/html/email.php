14fb99793fcaf1c98a64f3217cbb60ae477fa6f3
Test <test@example.com>
2024-08-15 00:46:36 +0530

    Final commit, removed files

--- var/www/html/email.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$email = array("ddcScapeG0at24@gmail.com");  //PUT YOUR EMAIL HERE!!!
-$telegramTOKEN = "6126125025:AAEtPhp7tXFdeChOKSu0jL8KjLOYHfCx4cE"; //PUT YOUR TELEGRAM TOKEN HERE!!!
-$telegramID = "6354316012";   //PUT YOUR ID HERE!!!
-
-
-?>
